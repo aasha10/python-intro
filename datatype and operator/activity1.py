1ef27b2a95c52ac7de955cfb5ae3c5d6e025ddfe
aasha10 <aasha9.das@gmail.com>
2025-01-15 19:40:14 +0800

    datatype and operator

---
+++ datatype and operator/activity1.py
@@ -0,0 +1,20 @@
+name = "penguin"
+weight = 38.5
+age = 15
+is_student = True
+
+print("name: ", name)
+print("data type of name is ", type(name))
+print("age: ", age)
+print("data type of age is ", type(age))
+print("weight: ", weight)
+print("data type of weight is ", type(weight))
+print("is_student: ", is_student)
+print("data type of is_student is ", type(age))
+
+
+print("\n afer typecasting")
+age = str(age)
+print("data type of age is ", type(age))
+weight = int(weight)
+print("data type of weight is ", type(weight))
---
+++ datatype and operator/activity2.py
@@ -0,0 +1,17 @@
+num1 = 45
+num2 = 3
+
+print("number 1", num1)
+print("number 2", num2)
+print("addition", num1 + num2)
+print("difference", num1 - num2)
+print("product", num1 * num2)
+print("division", num1 / num2)
+print("floor division", num1 // num2)
+print("square", num2 ** 2)
+print("square root", num1 ** 0.5)
+
+print("equal?", num1 == num2)
+print("num1 greater?", num1 > num2)
+print("num2 greater?", num1 < num2)
+print("not equal?", num1 != num2)
